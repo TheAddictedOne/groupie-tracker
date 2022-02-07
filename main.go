c81e8969867828e0664db36bbace3b0a3d8c1253
David Auk <auk.david@gmail.com>
2022-02-07 22:43:06 +0100

    Add GET to artist

--- main.go
+++ main.go
@@ -4,17 +4,20 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"text/template"
 )
 
-type IndexData struct {
+// Définitions des structures
+
+type URLs struct {
 	Artists   string `json:"artists"`
 	Locations string `json:"locations"`
 	Dates     string `json:"dates"`
 	Relation  string `json:"relation"`
 }
 
-type ArtistsData []struct {
+type Artist struct {
 	Id           int      `json:"id"`
 	Image        string   `json:"image"`
 	Name         string   `json:"name"`
@@ -27,14 +30,8 @@ type ArtistsData []struct {
 }
 
 type AllData struct {
-	IndexData   IndexData
-	ArtistsData ArtistsData
-}
-
-func main() {
-	http.HandleFunc("/", homepage)
-
-	http.ListenAndServe(":8080", nil)
+	URLs    URLs
+	Artists []Artist
 }
 
 func homepage(w http.ResponseWriter, r *http.Request) {
@@ -43,19 +40,42 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	// GET index
 	response, _ := http.Get("https://groupietrackers.herokuapp.com/api")
 	data, _ := ioutil.ReadAll(response.Body)
-	var indexData IndexData
-	json.Unmarshal(data, &indexData)
+	var urls URLs
+	json.Unmarshal(data, &urls)
 
 	// GET artists
-	response, _ = http.Get(indexData.Artists)
+	response, _ = http.Get(urls.Artists)
 	data, _ = ioutil.ReadAll(response.Body)
-	var artistsData ArtistsData
-	json.Unmarshal(data, &artistsData)
+	var artists []Artist
+	json.Unmarshal(data, &artists)
 
 	allData := AllData{
-		IndexData:   indexData,
-		ArtistsData: artistsData,
+		URLs:    urls,
+		Artists: artists,
 	}
 
 	page.Execute(w, allData)
+}
+
+func artist(w http.ResponseWriter, r *http.Request) {
+	page := template.Must(template.ParseFiles("views/artist.html"))
+	id := r.URL.Query().Get("id") // Parse "?id=""
+	source := strings.Replace("https://groupietrackers.herokuapp.com/api/artists/:id", ":id", id, 1)
+
+	// GET artist/:id
+	response, _ := http.Get(source)
+	data, _ := ioutil.ReadAll(response.Body)
+	var artist Artist
+	json.Unmarshal(data, &artist)
+
+	page.Execute(w, artist)
+}
+
+// Lancement du serveur web
+
+func main() {
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/artist", artist)
+
+	http.ListenAndServe(":8080", nil)
 }
