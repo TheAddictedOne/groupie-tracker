2172e28dc9abecdb89c4494594f1de9d7622cb04
David Auk <auk.david@gmail.com>
2022-02-07 21:47:47 +0100

    Fetch resource

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"text/template"
 )
+
+type IndexData struct {
+	Artists   string `json:"artists"`
+	Locations string `json:"locations"`
+	Dates     string `json:"dates"`
+	Relation  string `json:"relation"`
+}
 
 func main() {
 	http.HandleFunc("/", homepage)
@@ -13,7 +22,11 @@ func main() {
 
 func homepage(w http.ResponseWriter, r *http.Request) {
 	page := template.Must(template.ParseFiles("views/homepage.html"))
-	page.Execute(w, nil)
-	// lire un template avec des placeholders
-	// Remplacer les placeholders avec les données récupérées de l'API
+
+	response, _ := http.Get("https://groupietrackers.herokuapp.com/api")
+	data, _ := ioutil.ReadAll(response.Body)
+	var object IndexData
+	json.Unmarshal(data, &object)
+
+	page.Execute(w, object)
 }
