0d9876dbb1659d56f11b5eb0416e75e7daf8db14
David Auk <auk.david@gmail.com>
2022-02-07 21:37:05 +0100

    Create static homepage

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"text/template"
+)
 
 func main() {
-	fmt.Println("coucou")
+	http.HandleFunc("/", homepage)
 
+	http.ListenAndServe(":8080", nil)
+}
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	page := template.Must(template.ParseFiles("views/homepage.html"))
+	page.Execute(w, nil)
+	// lire un template avec des placeholders
+	// Remplacer les placeholders avec les données récupérées de l'API
 }
